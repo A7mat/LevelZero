40e6bdc0a17962d120994875ebe0e80cedb346f1
a7mat <ahmadasadi37@gmail.com>
2019-08-04 13:10:00 +0200

    added Intro and modified some text/conditions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,23 +6,43 @@ public class Main {
 
     // items to be used in all the rooms
     static int scissors = 0;
-    static int lighter = 0;
+    static int lighter = 1;
     static int socks = 0;
-    static int roomKey = 0;
+    static int roomKey = 1;
 
     static int greenLock = 0;            // Condition of the green door
     static int exitLevelZero = 0;        // Game ends when exitLevelZero = 1
 
 
+    public static void gameIntro() {
+        System.out.println("########################");
+        System.out.println("# WELCOME TO LEVELZERO #");
+        System.out.println("########################");
+        try {
+            Thread.sleep(700);
+            System.out.println("Your game will start in..");
+            Thread.sleep(700);
+            System.out.println(3);
+            Thread.sleep(1000);
+            System.out.println(2);
+            Thread.sleep(1000);
+            System.out.println(1);
+            Thread.sleep(1000);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void main(String[] args) {
+        //gameIntro();
 
         System.out.println("You are standing in a dark room, all you can see is three glowy doors: yellow, orange and green");
 
         int key = 0;
         while (key == 0) {
 
-            if (exitLevelZero == 1){            // exitLevelZero == 1 is the end condition of the game
+            if (exitLevelZero == 1) {            // exitLevelZero == 1 is the end condition of the game
                 return;
             }
             System.out.println("keywords are: open, yellow, orange, green.");
@@ -40,8 +60,7 @@ public class Main {
             // GREEN DOOR
             else if (door.equals("open green")) {
                 greenDoor(scanner);
-                }
-            else {
+            } else {
                 System.out.println("try another order");
             }
         }
@@ -54,7 +73,7 @@ public class Main {
     // 'exit's move the user back to the 3 doors
 
 
-             // Yellow Door
+    // Yellow Door
     public static void yellowDoor(Scanner scanner) {
         System.out.println("You entered the room. You see a quite man and a worried woman, it's your chance to make friends!");
 
@@ -91,7 +110,7 @@ public class Main {
                     }
                 }
             }
-            // Scenario 1
+            // Dialog 1
             else if (yellow.equals("talk to man")) {
 
                 System.out.println("Player: Where am I?");
@@ -101,11 +120,11 @@ public class Main {
                 System.out.println("Player: uh.. what??");
                 System.out.println("Man: DON'T YOU GET IT! IT'S NONE DIMENSIONAL");
                 System.out.println("Player: I.. ");
-                System.out.println("Man: DO YOU EVEN CHOOSE YOUR WORDS?");
+                System.out.println("Man: DO YOU EVEN CHOOSE YOUR OWN WORDS?");
                 System.out.println("\n ...the man got out of control, try another order");
 
             }
-            // Scenario 2
+            // Dialog 2
             else if (yellow.equals("talk to woman")) {
                 System.out.println("Woman: sorry young man, if I help you they might remove me from the..");
                 System.out.println("Player: remove you! what the hell is that supposed to mean?!");
@@ -122,7 +141,7 @@ public class Main {
         }
     }
 
-     // Orange Door
+    // Orange Door
     public static void orangeDoor(Scanner scanner) {
         int orangeKey = 0;
         System.out.println("You entered the room, but it's so dark you can't see anything!");
@@ -135,11 +154,9 @@ public class Main {
             // Checks if user gathered the lighter from the other room in order to use it
             if (lighter == 0 && orange.equals("use lighter")) {
                 System.out.println("you don't have a lighter");
-            }
-            else if (orange.equals("use light")) {
+            } else if (orange.equals("use light")) {
                 System.out.println("light is broken");
-            }
-            else if (lighter == 1 && orange.equals("use lighter")) {
+            } else if (lighter == 1 && orange.equals("use lighter")) {
                 System.out.println("Impressive! now that you can see what's around you, you can navigate the room");
 
                 // Moving around in the Orange room
@@ -157,41 +174,44 @@ public class Main {
                             System.out.println("Keywords: check, exit, under, rug, room");
                             String rug = scanner.nextLine();
                             if (rug.equals("check under rug")) {
-                                System.out.println("under the rug you find a key and much of dirt");
+                                if (roomKey == 0) {
+                                    System.out.println("under the rug you find a key and much of dirt");
 
-                                int underRug = 0;
-                                while (underRug == 0) {
-                                    System.out.println("Keywords: take, key");
-                                    String takeKey = scanner.nextLine();
-                                    if (takeKey.equals("take key")) {
-                                        System.out.println("taken");
-                                        roomKey = 1;
-                                        underRug = 1;
-                                        north = 1;
+                                    int underRug = 0;
+                                    while (underRug == 0) {
+                                        System.out.println("Keywords: take, key");
+                                        String takeKey = scanner.nextLine();
+                                        if (takeKey.equals("take key")) {
+                                            System.out.println("taken");
+                                            roomKey = 1;
+                                            underRug = 1;
+                                            north = 1;
 
-                                    } else if (takeKey.equals("back")) {
-                                        underRug = 1;
-                                    } else if (takeKey.equals("exit room") || takeKey.equals("exit")) {
-                                        System.out.println("you are back to the room with the doors, yellow, orange and green");
-                                        underRug = 1;
-                                        orientationKey = 1;
-                                        orangeKey = 1;
-                                        north = 1;
-                                    } else {
-                                        System.out.println("try another order");
+                                        } else if (takeKey.equals("back")) {
+                                            underRug = 1;
+                                        } else if (takeKey.equals("exit room") || takeKey.equals("exit")) {
+                                            System.out.println("you are back to the room with the doors, yellow, orange and green");
+                                            underRug = 1;
+                                            orientationKey = 1;
+                                            orangeKey = 1;
+                                            north = 1;
+                                        } else {
+                                            System.out.println("try another order");
+                                        }
                                     }
                                 }
-                            }
-                            else if (rug.equals("back")) {
+                                else {      // Key already taken
+                                    System.out.println("dirt, and only dirt.");
+                                    north = 1;
+                                }
+                            } else if (rug.equals("back")) {
                                 north = 1;
-                            }
-                            else if (rug.equals("exit room") || rug.equals("exit")) {
+                            } else if (rug.equals("exit room") || rug.equals("exit")) {
                                 System.out.println("you are back to the room with the doors, yellow, orange and green");
                                 orientationKey = 1;
                                 orangeKey = 1;
                                 north = 1;
-                            }
-                            else {
+                            } else {
                                 System.out.println("try another order");
                             }
                         }
@@ -204,25 +224,22 @@ public class Main {
                     else if (orientation.equals("go to east")) {
                         System.out.println("You see a crooked painting on the wall");
                         int eastKey = 0;
-                        while (eastKey == 0){
+                        while (eastKey == 0) {
                             System.out.println("Keywords: remove, exit, painting, room ,back");
                             String painting = scanner.nextLine();
-                            if (painting.equals("remove painting")){
+                            if (painting.equals("remove painting")) {
                                 System.out.println("on the back of the painting you find a piece of paper with the following note:" +
                                         " \"PASSWORD: three\"");
                                 eastKey = 1;
 
-                            }
-                            else if (painting.equals("back")){
+                            } else if (painting.equals("back")) {
                                 eastKey = 1;
-                            }
-                            else if (painting.equals("exit room") || painting.equals("exit")) {
+                            } else if (painting.equals("exit room") || painting.equals("exit")) {
                                 System.out.println("you are back to the room with the doors, yellow, orange and green");
                                 eastKey = 1;
                                 orientationKey = 1;
                                 orangeKey = 1;
-                            }
-                            else {
+                            } else {
                                 System.out.println("try another order");
                             }
                         }
@@ -230,12 +247,9 @@ public class Main {
                     // West
                     else if (orientation.equals("go to west")) {
                         System.out.println("You only see smelly garbage, nothing interesting here!");
-                    }
-                    else if (orientation.equals("back")) {
+                    } else if (orientation.equals("back")) {
                         orientationKey = 1;
-                    }
-
-                    else if (orientation.equals("exit room") || orientation.equals("exit")) {
+                    } else if (orientation.equals("exit room") || orientation.equals("exit")) {
                         System.out.println("you are back to the room with the doors, yellow, orange and green");
                         orientationKey = 1;
                         orangeKey = 1;
@@ -265,7 +279,9 @@ public class Main {
                 System.out.println("Keywords: use, back, key");
 
                 lock = scanner.nextLine();
-            } else {lock = "use key";}
+            } else {
+                lock = "use key";
+            }
 
 
             if (lock.equals("use key") && roomKey == 1) {
@@ -290,13 +306,14 @@ public class Main {
                                 System.out.println("LevelZero.java, readMe.txt, Recycle Bin");
 
                                 int desktop = 0;
-                                while (desktop == 0){
+                                while (desktop == 0) {
 
 
-                                    System.out.println("Keywords: open, back, exit read me, levelzero, recycle bin, room");
+                                    System.out.println("Keywords: open, back, exit, read me, levelzero, recycle bin, room");
 
                                     String programs = scanner.nextLine();
-                                    if(programs.equals("open levelzero")){
+                                    // LevelZero.java
+                                    if (programs.equals("open levelzero")) {
                                         System.out.println("\n\npublic class LevelZero {\n  public static void main(String[] args){\n\n      int exitLevelZero = 0;\n      " +
                                                 "boolean playerIsTrapped = true;\n" +
                                                 "    while(exitLevelZero == 0){\n\n      }\n      playerIsTrapped = false;\n  }\n}");
@@ -304,37 +321,42 @@ public class Main {
                                         int deepestWhile = 0;
                                         while (deepestWhile == 0) {
                                             String last = scanner.nextLine();
-                                            if (last.equals("exitLevelZero++;")|| last.equals("exitLevelZero = 1;")|| last.equals("exitLevelZero=1;")
-                                            || last.equals("playerIsTrapped=false;") || last.equals("playerIsTrapped = false;")){
+                                            if (last.equals("exitLevelZero++;") || last.equals("exitLevelZero = 1;") || last.equals("exitLevelZero=1;")
+                                                    || last.equals("playerIsTrapped=false;") || last.equals("playerIsTrapped = false;")) {
                                                 System.out.println(".\n====================================================================");
-                                                System.out.println("Congrats! you figured your way out of LevelZero! Now back to the Real game" +
-                                                        ", do you have a clue? it's three dimensional!\n==================================" +
+                                                System.out.println("Congrats! you figured your way out of LevelZero!\n==================================" +
                                                         "==================================\n.");
+                                                try {
+                                                    Thread.sleep(1000);
+                                                } catch (InterruptedException e) {
+                                                    e.printStackTrace();
+                                                }
+                                                System.out.println("Now back to the Real game" +
+                                                        ", do you have a clue? this one is THREE DIMENSIONAL!");
                                                 exitLevelZero++;
                                                 return;
-                                            }
-                                            else if (last.equals("back")){
+                                            } else if (last.equals("back")) {
                                                 deepestWhile = 1;
-                                            }
-                                            else if (last.equals("exit") || last.equals("exit room")) {
+                                            } else if (last.equals("exit") || last.equals("exit room")) {
                                                 System.out.println("you are back to the room with the doors, yellow, orange and green");
                                                 deepestWhile = 1;
                                                 desktop = 1;
                                                 enterPass = 1;
                                                 greenKey = 1;
-                                            }
-                                            else {
+                                            } else {
                                                 System.out.println("try another order");
                                             }
                                         }
-                                    } else if(programs.equals("open read me")){
-                                        System.out.println("You will be missed. (not really)");
+                                    }
+                                    // readMe.txt
+                                    else if (programs.equals("open read me")) {
+                                        System.out.println("\"You will be missed. (not really)\"");
 
                                         int readMeKey = 0;
                                         while (readMeKey == 0) {
                                             System.out.println("Keywords: back, exit, room");
                                             String quitReadMe = scanner.nextLine();
-                                            if (quitReadMe.equals("back")){
+                                            if (quitReadMe.equals("back")) {
                                                 readMeKey = 1;
                                             } else if (quitReadMe.equals("exit") || quitReadMe.equals("exit room")) {
                                                 System.out.println("you are back to the room with the doors, yellow, orange and green");
@@ -347,7 +369,9 @@ public class Main {
                                             }
                                         }
 
-                                    } else if(programs.equals("open recycle bin")){
+                                    }
+                                    // Recycle Bin
+                                    else if (programs.equals("open recycle bin")) {
                                         System.out.println("*empty folder*");
 
                                         int binKey = 0;
@@ -364,7 +388,7 @@ public class Main {
                                                 greenKey = 1;
                                             }
                                         }
-                                    } else if(programs.equals("back")){
+                                    } else if (programs.equals("back")) {
                                         desktop = 1;
                                     } else {
                                         System.out.println("try another order");
@@ -388,16 +412,13 @@ public class Main {
                     }
                 }
                 keyLock = 1;
-            }
-            else if (lock.equals("use key") && roomKey == 0) {
+            } else if (lock.equals("use key") && roomKey == 0) {
                 System.out.println("you don't have the key");
                 keyLock = 1;
-            }
-            else if (lock.equals("back")) {
+            } else if (lock.equals("back")) {
                 System.out.println("you are back to the room with the doors, yellow, orange and green");
                 break;
-            }
-            else {
+            } else {
                 System.out.println("try another order");
             }
         }
